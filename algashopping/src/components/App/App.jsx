c0c30cf452cd5bf569ab5fd941a38cba1942bdc1
Vinicius Smith <80126382+ViniSmith@users.noreply.github.com>
2022-06-09 14:46:27 -0300

    Realizando uma estruturação de estilo

---
+++ algashopping/src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Wrapper } from './App.styles'
+
+const App = () => {
+    return <Wrapper>
+        <Container>
+            todo: app
+        </Container>
+    </Wrapper>
+}
+
+export default App
---
+++ algashopping/src/components/App/App.styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import bg from '../../assets/bg.svg'
+export const Wrapper = styled.div`
+    height: 100vh;
+    background-color: #F8F5EC;
+    background-image: url(${bg});
+    background-size: cover;
+    background-position: center center;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+
+export const Container = styled.div`
+    width: 734px;
+    height: 574px;
+    background-color: white;
+`
